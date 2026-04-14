95411dd7c20f4ef0e1100d62ea9fa216ae7829a0
Mac <mac@PHANTOM.local>
2026-04-14 11:06:59 +0530

    feat : banking sys working test

--- basic_essaging.py
+++ basic_essaging.py
@@ -1,10 +1,12 @@
 from langchain_groq import ChatGroq
-from langchain.messages import SystemMessage,HumanMessage
+from langchain.messages import SystemMessage,HumanMessage, AIMessage
 model = ChatGroq(model="llama-3.1-8b-instant")
 
 
 sys_msg = SystemMessage("You are a helpfull agent")
 hum_msg = HumanMessage("tell me a joke")
+ai_msg = AIMessage("Why did the programmer quit his job? Because he didn't get arrays.")
+
 
 message = [sys_msg,hum_msg]
 
